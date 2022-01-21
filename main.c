5e8090d68183d01f58b28332d103e75fe28c0e3d
parsa333111 <98084810+parsa333111@users.noreply.github.com>
2022-01-21 22:28:52 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,61 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include "menu.h"
+#include "change string.h"
 
 int row = 37;
 
 struct hexagon {
-    int x, y;
+    int xlight, ylight;
+    int xhouse, yhouse;
+    int xwell, ywell;
     int type;
-    bool on_open;
-
+    bool on;
+    bool open;
+    int character;
 };
 /*
 type :
 1 = house
-2 = lamp
+2 = light
 3 = water well
 4 = escape
 */
+struct hexagon hex[13][9];
+
+void assigned() {
+    for(int i = 0 ; i < 13 ; i++ ) {
+        for(int j = 0 ; j < 9 ; j++ ) {
+            hex[i][j].type = 0;
+        }
+    }
+    hex[1][0].type = hex[1][8].type = hex[11][0].type = hex[11][8].type = 4;
+}
+
+void create_light_xy() {
+    hex[0][0].ylight = 4, hex[0][0].xlight = 1;
+    for(int i = 0 ; i <= 8 ; i++) {
+        for(int j = 0 ; j <= 12 ; j+=2) {
+            if(i == 0 && j == 0)continue;
+            hex[j][i].xlight = hex[0][0].xlight + (8 * j);
+            hex[j][i].ylight = hex[0][0].ylight + (4 * i);
+        }
+    }
+    hex[1][0].ylight = 2, hex[1][0].xlight = 9;
+    for(int i = 0 ; i <= 8 ; i++) {
+        for(int j = 1 ; j <= 12 ; j+=2) {
+            if(i == 1 && j == 0)continue;
+            hex[j][i].xlight = hex[1][0].xlight + (8 * j) - 8;
+            hex[j][i].ylight = hex[1][0].ylight + (4 * i);
+        }
+    }
+}
+
 int main () {
+    assigned();
+    create_light_xy();
     while(true) {
         print_first_menu();
         int op;
@@ -35,7 +72,6 @@ int main () {
                 print_custom_map_menu();
                 int op2;
                 scanf("%d", &op2);
-                getchar();
                 FILE *fp = fopen("row board.txt", "r");
                 char str[40][110];
                 for(int i = 0 ; i < row ; i++ ) {
@@ -51,12 +87,31 @@ int main () {
                 fclose(fp);
                 getchar();
                 if(op2 == 1) {
-                    while(true) {
+                    int mande;
+                    mande = 6;
+                    while(mande) {
+                        system("cls");
                         printf("current map\n");
                         for(int i = 0 ; i < row ; i++ ) {
                             puts(str[i]);
                         }
-
+                        printf("You must add %d light on\n", mande);
+                        printf("Enter X, y for add light on : ");
+                        int x, y;
+                        scanf("%d%d", &x, &y);
+                        if(hex[x][y].type != 0) {
+                            printf("this position isn't empty try again\n");
+                        }
+                        else {
+                            printf("%d %d\n", hex[x][y].ylight, hex[x][y].xlight);
+                            hex[x][y].type = 2;
+                            hex[x][y].on = 1;
+                            mande--;
+                            change(str[hex[x][y].ylight], "lamp on", hex[x][y].xlight);
+                        }
+                        getchar();
+                        printf("press enter for continue build ,ap\n");
+                        getchar();
                     }
                 }
                 else if(op2 == 2) {
