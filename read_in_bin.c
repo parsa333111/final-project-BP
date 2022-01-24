c23a3dad44ff4f5141a3147708804bb7eb5cafba
parsa333111 <98084810+parsa333111@users.noreply.github.com>
2022-01-24 14:19:54 +0330

    Create read_in_bin.c

---
+++ read_in_bin.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+//100 dorchand turn[1,4]
+int main () {
+    FILE *f = fopen("back default map.txt", "rb");
+    int endl = 0;
+    while(1) {
+        int x;
+        if(fread(&x, 4, 1, f) ==0) break;
+        printf("%d ", x);
+        endl++;
+        if(endl % 5 == 0) {
+            printf("\n");
+        }
+    }
+    fclose(f);
+}
