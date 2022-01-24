ce8218ec0085c4c78c311c1876334c795d7a707e
parsa333111 <98084810+parsa333111@users.noreply.github.com>
2022-01-24 22:18:11 +0330

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,17 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include "menu.h"
+#include <time.h>
 #include "change string.h"
 #include "character.h"
+#include "menu.h"
 
 int row = 37;
+
+struct node {
+    int id;
+    struct node *nex;
+};
 
 struct hexagon {
     int xlight, ylight;
@@ -105,7 +111,103 @@ int *give_pointer(int x) {
     }
 }
 
+void add_in_list(int i, struct node *list) {
+    while(list -> nex != NULL) {
+        list = list -> nex;
+    }
+    struct node *new = malloc(sizeof(struct node*));
+    new -> id = i;
+    list -> nex = new;
+    new -> nex = NULL;
+}
+
+void shuffle(struct node *list, int sz) {
+    struct node *cpy_list = list;
+    for(int i = 1 ; i <= 8 ; i++ ) {
+        list = cpy_list;
+        int size = i;
+        int j = rand() % size;
+        j++;
+        int cpy_i = i - 1, cpy_j = j - 1;
+        int val_i, val_j;
+        while(cpy_i--) {
+            list = list -> nex;
+        }
+        val_i = list -> id;
+        list = cpy_list;
+        while(cpy_j--) {
+            list = list -> nex;
+        }
+        val_j = list -> id;
+        list -> id = val_i;
+        //
+        list = cpy_list;
+        cpy_i = i - 1;
+        while(cpy_i--) {
+            list = list -> nex;
+        }
+        list -> id = val_j;
+    }
+}
+
+struct node* next_four(struct node *list) {
+    for(int i = 0 ; i < 3 ; i++ ) {
+        list = list -> nex;
+    }
+    struct node *list2 = list -> nex;
+    list -> nex = NULL;
+    return list2;
+}
+
+void print_board(char str[40][110]) {
+    for(int i = 0 ; i < row ; i++ ) {
+        puts(str[i]);
+    }
+}
+
+void print_list(struct node *list, int x) {
+    x--;
+    while(x--) {
+        list = list -> nex;
+    }
+    while(list != NULL) {
+        print_char(list -> id);
+        printf(" ");
+        list = list -> nex;
+    }
+    printf("\n");
+}
+
+void print_turn(int dor, int tur) {
+    if(dor % 2 == 1) {
+        if(tur == 1 || tur == 4) {
+            printf("detective turn\n");
+        }
+        else {
+            printf("mr.jack turn\n");
+        }
+    }
+    else {
+        if(tur == 2 || tur == 3) {
+            printf("detective turn\n");
+        }
+        else {
+            printf("mr.jack turn\n");
+        }
+    }
+}
+
+bool exist_list(struct node* list, int find, int ign) {
+    while(ign--) list = list -> nex;
+    while(list != NULL) {
+        if(list -> id == find) return 1;
+        list = list -> nex;
+    }
+    return 0;
+}
+
 int main () {
+    srand(time(0));
     create();
     while(true) {
         assigned();
@@ -116,8 +218,9 @@ int main () {
         if(op == 1 || op == 2) {
             //type x y use tartib
             //100 dorchand turn[1,4]
-            int dor, turn, vis, char_on_table[4] = {-1,-1,-1,-1};
+            int dor, turn, vis, char_on_table[8] = {-1,-1,-1,-1,-1,-1,-1,-1}, jack = 0;
             bool char_on_board = 0;
+            char str[40][110];
             if(op == 1) {
                 while(true) {
                     print_start_new_game_map();
@@ -126,10 +229,10 @@ int main () {
                     if(op2 == 10)break;
                     getchar();
                     FILE *back_map, *front_map;
-                    char str[40][110];
                     if(op2 < 0 || op2 > 9) {
                         printf("Wrong input press enter and try again\n");
                         getchar();
+                        continue;
                     }
                     else if(op2 == 0) {
                         back_map = fopen("back default map.txt", "rb");
@@ -154,6 +257,7 @@ int main () {
                     while(fread(&p, 4, 1, back_map) != 0) {
                         if(cnt > 0) {
                             stack[cnt] = p;
+                            cnt++;
                         }
                         else if(cnt == 0) {
                             if(p != 100) {
@@ -185,6 +289,8 @@ int main () {
                         cnt++;
                         char_on_board = 1;
                     }
+                    if(0 == fread(&jack, 4, 1, back_map)) jack = 0;
+                    break;
                 }
             }
             else {
@@ -195,7 +301,6 @@ int main () {
                     if(op2 == 10)break;
                     getchar();
                     FILE *back_map, *front_map;
-                    char str[40][110];
                     if(op2 < 1 || op2 > 9) {
                         printf("Wrong input press enter and try again\n");
                         getchar();
@@ -219,6 +324,7 @@ int main () {
                     while(fread(&p, 4, 1, back_map) != 0) {
                         if(cnt > 0) {
                             stack[cnt] = p;
+                            cnt++;
                         }
                         else if(cnt == 0) {
                             if(p != 100) {
@@ -250,16 +356,66 @@ int main () {
                         cnt++;
                         char_on_board = 1;
                     }
+                    if(0 == fread(&jack, 4, 1, back_map)) jack = 0;
+                    break;
                 }
             }
-            if(char_on_board) {
-                /*
-                fill it
-                */
-            }
-            else {
+            //group 1, group 2
+            {
+                struct node *list1 = NULL, *list2 = NULL;
                 for(; dor <= 8 ; dor++) {
+                     //change list and free them if(dor % 2 == 1) list1 = list2 = NULL;
+                    for(int tur = turn ; tur <= 4 ; tur++ ) {
+                         if(char_on_board) {
+                            char_on_board = 0;
+                            for(int i = 1 ; i <= 8 ; i++ ) {
+                                if(i == 1) {
+                                    list1 = malloc(sizeof(struct node*));
+                                    list1 -> nex = NULL;
+                                    list1 -> id = char_on_table[i-1];
+                                }
+                                else {
+                                    add_in_list(char_on_table[i-1], list1);
+                                }
+                            }
+                            list2 = next_four(list1);
+                         }
+                         else if (tur == 1) {
+                            for(int i = 1 ; i <= 8 ; i++ ) {
+                                if(i == 1) {
+                                    list1 = malloc(sizeof(struct node*));
+                                    list1 -> nex = NULL;
+                                    list1 -> id = i;
+                                }
+                                else {
+                                    add_in_list(i, list1);
+                                }
+                            }
 
+                            shuffle(list1, 8);
+                            list2 = next_four(list1);
+                        }
+                        while(true) {
+                            system("cls");
+                            print_board(str);
+                            print_turn(dor, tur);
+                            print_choosing_menu();
+                            if(dor % 2 == 1) print_list(list1, tur);
+                            else print_list(list2, tur);
+                            struct node *list = (dor % 2 == 0) ? list2 : list1;
+                            int choose_char;
+                            scanf("%d", &choose_char);
+                            if(exist_list(list, choose_char, tur - 1)) {
+
+                            }
+                            else {
+                                printf("Wrong input press Enter and try again\n");
+                                getchar();
+                                getchar();
+                            }
+                        }
+                    }
+                    turn = 1;
                 }
             }
         }
